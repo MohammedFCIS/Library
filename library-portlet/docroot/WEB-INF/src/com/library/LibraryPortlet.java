3bc0aa949e90327ab7dbabb22132d189ec04e280
Mohammed Ali <moh_fcis@yahoo.com>
2015-12-24 13:23:12 +0200

    add updateBook action

--- library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
+++ library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
@@ -1,11 +1,23 @@
 package com.library;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class LibraryPortlet
  */
 public class LibraryPortlet extends MVCPortlet {
- 
-
+	public void updateBook(ActionRequest request, ActionResponse response)
+	throws IOException, PortletException{
+		String bookTitle = ParamUtil.getString(request, "bookTitle");
+		String bookAuthor = ParamUtil.getString(request, "bookAuthor");
+		System.out.println("bookTitle:" + bookTitle);
+		System.out.println("bookAuthor:" + bookAuthor);
+	}
 }
